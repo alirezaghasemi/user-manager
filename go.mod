b09898af0d5f95ddf50f62eb823d27c74003ea2b
alirezaghasemi <ghasemial@yahoo.com>
2025-07-04 00:02:41 +0330

    make test for usecase

--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.23.4
 
 require (
 	github.com/gin-gonic/gin v1.10.1
+	github.com/go-playground/validator/v10 v10.26.0
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pressly/goose/v3 v3.24.3
 	github.com/spf13/cobra v1.9.1
+	github.com/stretchr/testify v1.10.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.30.0
 )
@@ -18,11 +20,11 @@ require (
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -34,14 +36,15 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
---
+++ internal/usecase/user_usecase_test.go
@@ -0,0 +1,97 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/alirezaghasemi/user-manager/internal/entities"
+	"github.com/alirezaghasemi/user-manager/internal/repository"
+	"github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) FindByID(ctx context.Context, id uint64) (entities.User, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(entities.User), args.Error(1)
+}
+
+func (m *MockUserRepository) Save(ctx context.Context, user entities.User) (entities.User, error) {
+	args := m.Called(ctx, user)
+	return args.Get(0).(entities.User), args.Error(1)
+}
+
+func (m *MockUserRepository) Update(ctx context.Context, user entities.User) (entities.User, error) {
+	args := m.Called(ctx, user)
+	return args.Get(0).(entities.User), args.Error(1)
+}
+
+func (m *MockUserRepository) FindAll(ctx context.Context) ([]entities.User, error) {
+	args := m.Called(ctx)
+	return args.Get(0).([]entities.User), args.Error(1)
+}
+
+func (m *MockUserRepository) Delete(ctx context.Context, id uint64) error {
+	args := m.Called(ctx)
+	return args.Error(0)
+}
+
+func TestUserUsecase_FindByID(t *testing.T) {
+	// make instance of validator
+	validate := validator.New()
+
+	mockRepo := &MockUserRepository{}
+
+	userUsecase := NewUserUsecase(mockRepo, validate)
+
+	// definition variables for test
+	ctx := context.Background()
+	userID := uint64(1)
+	expectedUser := entities.User{
+		ID:     userID,
+		Name:   "Test User",
+		Family: "Test Family",
+		Email:  "test@gamil.com",
+		Age:    31,
+	}
+
+	// setup expectations
+	t.Run("Success", func(t *testing.T) {
+		mockRepo.On("FindByID", ctx, userID).Return(expectedUser, nil).Once()
+
+		user, err := userUsecase.FindByID(ctx, userID)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedUser, user)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("UserNotFound", func(t *testing.T) {
+		mockRepo.On("FindByID", ctx, userID).Return(entities.User{}, repository.ErrMsgUserNotFound).Once()
+
+		user, err := userUsecase.FindByID(ctx, userID)
+
+		assert.Error(t, err)
+		assert.True(t, errors.Is(err, ErrMsgUserNotFound))
+		assert.Equal(t, entities.User{}, user)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("InternalServerError", func(t *testing.T) {
+		genericError := errors.New("database error")
+
+		mockRepo.On("FindByID", ctx, userID).Return(entities.User{}, genericError).Once()
+
+		user, err := userUsecase.FindByID(ctx, userID)
+
+		assert.Error(t, err)
+		assert.True(t, errors.Is(err, ErrMsgInternalServerError))
+		assert.Equal(t, entities.User{}, user)
+		mockRepo.AssertExpectations(t)
+	})
+}
